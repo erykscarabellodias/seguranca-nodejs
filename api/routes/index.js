53e34239f535f03bf091d4cabdbf63e35eb36c7d
erykscarabellodias <erykscarabello97@gmail.com>
2023-06-13 15:08:33 -0300

    feat: criando rotas de usuários

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,13 @@
 const bodyParser = require('body-parser')
- 
+
 const produto = require('./produtoRoute')
+
+const usuario = require('./usuariosRoute');
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
-    produto
+    produto,
+    usuario
   )
 }
