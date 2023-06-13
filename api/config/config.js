46014c75845f01967c1842043ec27fa44a4a5913
erykscarabellodias <erykscarabello97@gmail.com>
2023-06-13 15:53:33 -0300

    refactor: adicionando variáveis de ambiente ao projeto

---
+++ api/config/config.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  }
+};
